c9a41a934c3677f469c26bc9ea618f48336e9076
Chaya Lent <mushkielent@gmail.com>
2023-12-19 11:37:34 -0500

    home page - view+play playlists

--- src/components/AlbumListItem.vue
+++ src/components/AlbumListItem.vue
@@ -1,25 +1,46 @@
 <script setup>
-defineProps({
+import { ref } from 'vue';
+import {embedplaybacksdk} from '../helpers/playback.js';
+
+const props = defineProps({
   playlist: {
     type: Object,
     required: true
+  }, 
+  play: {
+    type: Function,
+    required: true
   }
 })
+const showplay = ref(false)
+const showplaybtn = () => {
+  // timeout 2 milliseconds 
+  setTimeout(()=>{showplay.value=true},200);
+}
+const hideplaybtn = () => {
+  // timeout 2 milliseconds 
+  setTimeout(()=>{showplay.value=false},20);
+}
 </script>
 
 <template>
-    <div style="
-  display: flex; flex-direction: column; padding-bottom: 10px;
-  justify-content: center; /* Centers horizontally */
-  align-items: center; text-align: center;">
-        <img :src="playlist.images[0].url" width="100" style="margin: 10px;"/>
-        <p>{{ playlist.name }}</p>
-
-    </div>
+    <article style="display: flex; flex-direction: column; padding-bottom: 10px; justify-content: center; /* Centers horizontally */ align-items: center; text-align: center; position: relative;">
+        <div v-on:mouseover="showplaybtn()" v-on:mouseleave="hideplaybtn()" @click="handleAlbumclick()"><img id="playlistimg" :src="playlist.images[0].url" width="200px" style="margin: 10px;">
+        <p v-if="showplay" style="position: absolute; top: 40%; left: 50%; transform: translate(-50%,-50%);">play</p></div>
+        <h4 style="width:200px;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;">{{ playlist.name }}</h4>
+        
+    </article>
 
 </template>
 
 <style scoped>
+#playlistimg:hover {
+  /* width: 80%; */
+  transform: scale(1.02);
+  transition: all .3s ease-in-out;
+  /* overlay color gray  */
+  filter: blur(3px);
+}
 </style>
 
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,97 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import AlbumListItem from '@/components/AlbumListItem.vue';
+import { fetchPlaylists, fetchProfile, playPlaylist, pausePlayback, switchPlaybackToLocalDevice } from '@/helpers/apifunctions';
+import { embedplaybacksdk } from '@/helpers/playback';
+import { ref } from 'vue';
+
+
+const username = ref('')
+const userid = ref(null)
+const playlists = ref([])
+const spotifyPlayer = ref(null)
+const isPlaying = ref(false)
+let currentToken = localStorage.getItem('spotify_access_token');
+
+
+await loadPageData()
+
+async function loadPageData() {
+  const profile = await fetchProfile(currentToken);
+  username.value = profile.display_name
+  userid.value = profile.id
+  const playlistsdata = await fetchPlaylists()
+  playlists.value = playlistsdata.items
+}
+
+embedplaybacksdk()
+window.onSpotifyWebPlaybackSDKReady = () => {
+  const token = localStorage.getItem("spotify_access_token");
+  spotifyPlayer.value = new Spotify.Player({
+    name: "Web Playback SDK Quick Start Player",
+    getOAuthToken: (cb) => {
+      cb(token);
+    },
+    volume: 0.5,
+  });
+
+  //   app.config.globalProperties.$spotifyPlayer = player;
+  localStorage.setItem("spotify-player", JSON.stringify("player"));
+  // Ready
+  spotifyPlayer.value.addListener("ready", ({ device_id }) => {
+    localStorage.setItem('device_id',device_id);
+  });
+
+  // Not Ready
+  spotifyPlayer.value.addListener("not_ready", ({ device_id }) => {
+    console.log("Device ID has gone offline", device_id);
+  });
+
+  spotifyPlayer.value.addListener("initialization_error", ({ message }) => {
+    console.error(message);
+  });
+
+  spotifyPlayer.value.addListener("authentication_error", ({ message }) => {
+    console.error(message);
+  });
+
+  spotifyPlayer.value.addListener("account_error", ({ message }) => {
+    console.error(message);
+  });
+
+  //document.getElementById("togglePlay").onclick = function () {
+    //console.log("toggleplay", spotifyPlayer.value);
+    // spotifyPlayer.value.togglePlay();
+  //};
+
+  spotifyPlayer.value.connect();
+  spotifyPlayer.value.setName("Mushkies Web Client");
+  switchPlaybackToLocalDevice(localStorage.getItem('device_id'))
+};
+const spotifyPlay = (playlistUri) => { 
+  playPlaylist(playlistUri)
+  isPlaying.value=playlistUri
+}
+const pause = () => {
+  pausePlayback()
+  .catch((err)=>{
+    console.log('could not pause', err)
+  }).finally(()=> {
+    isPlaying.value=false
+  })
+}
+
+const handlePlaylistClick = (playlistUri) => {
+  console.log('handleplaylistclick',playlistUri,isPlaying.value);
+  isPlaying.value && isPlaying.value==playlistUri ? pause() : spotifyPlay(playlistUri)
+}
+
 </script>
 
 <template>
   <main>
-      <h1>you should be redirected to login to spotify.</h1>
-    </main>
+    <p style="position: absolute;top:10px;right:10px">HELLO {{ username }}</p>
+    <div style="display: grid; grid-template-columns: repeat(auto-fit, minmax(200px, 1fr)); grid-gap: 1rem;">
+      <AlbumListItem v-for="playlist in playlists" :playlist="playlist" :play="spotifyPlay" @click="handlePlaylistClick(playlist.uri)" />
+   </div>
+  </main>
 </template>
